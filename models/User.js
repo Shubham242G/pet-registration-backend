847441ec6a38a51f8f743ee806edbfe3391bb3c1
Shubham242G <allhailvicky242@@gmail.com>
2026-02-27 14:40:18 +0530

    added lice backend URL

--- models/User.js
+++ models/User.js
@@ -25,3 +25,5 @@ userSchema.pre(/^find/, function() {
 userSchema.methods.comparePassword = async function(password) {
   return await bcrypt.compare(password, this.password);
 };
+
+module.exports = mongoose.model('User', userSchema);
